11daae0efaef19657851f04348f1057d334abcb4
cedric leong <cedricleong@gmail.com>
2015-11-02 16:30:19 -0500

    Fix duplicate product grid

--- app/code/community/TBT/Enhancedgrid/controllers/Catalog/ProductController.php
+++ app/code/community/TBT/Enhancedgrid/controllers/Catalog/ProductController.php
@@ -46,11 +46,6 @@ class TBT_Enhancedgrid_Catalog_ProductController extends Mage_Adminhtml_Catalog_
     {
         $this->loadLayout();
         $this->_setActiveMenu('catalog/enhancedgrid');
-
-        $this->_addContent(
-            $this->getLayout()->createBlock('enhancedgrid/catalog_product')
-        );
-
         $this->renderLayout();
     }
 
